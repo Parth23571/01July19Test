0f92f326fd20a47b1ec4bcfa820012a557487345
parth235 <pawal.pankaj@gmail.com>
2019-07-01 16:06:18 +0530

    First COmmit

---
+++ sel1.test1/src/test/java/sel1/test1/openBrowser/OpenBrowser1.java
@@ -0,0 +1,30 @@
+package sel1.test1.openBrowser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/*
+ * 
+ */
+public class OpenBrowser1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		try
+		{
+			System.out.println("Started---");
+			System.setProperty("webdriver.chrome.driver","C:\\Browers\\chromedriver.exe");
+			WebDriver driver=new ChromeDriver();
+			driver.get("http://localhost:8080");
+			driver.manage().window().maximize();
+			System.out.println("Stopped---");
+		}
+		
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
